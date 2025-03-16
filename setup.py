05a75ccb1a437af6a4ca06efc656839696ef037d
Panbhuarasane-A <panbhuofficial@gmail.com>
2025-03-16 18:54:25 +0530

    folder structure add

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='genrative ai project',
+    version='0.0.0',
+    author='panbhu',
+    author_email='pant3456q@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
